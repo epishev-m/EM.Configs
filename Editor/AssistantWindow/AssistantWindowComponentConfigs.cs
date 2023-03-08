f87c3c15071965d7d719e8608af4fe1662d73e15
Maxim Epishev <epishev.m@mail.ru>
2023-03-08 17:02:40 +0300

    Minore Refactoring

--- Editor/AssistantWindow/AssistantWindowComponentConfigs.cs
+++ Editor/AssistantWindow/AssistantWindowComponentConfigs.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Assistant.Editor;
 using EM.Foundation.Editor;
 using MessagePack;
 using Newtonsoft.Json;
